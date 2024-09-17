f25fa9fe027511dee0f77df4247858153a047541
marwanhesham88 <marwanhesham115@gmail.com>
2024-09-17 13:45:15 +0300

    second commit

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -86,7 +86,7 @@ export default function Cart() {
     <h2 className='text-center text-2xl text-emerald-600 font-bold capitalize my-4'>total price: {cartDetails?.totalCartPrice} EGP</h2>
 <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
   <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
       <tr>
         <th scope="col" className="px-16 py-3">
           <span className="sr-only">Image</span>
@@ -105,7 +105,7 @@ export default function Cart() {
         </th>
       </tr>
     </thead>
-    <tbody>
+    <tbody className=''>
       {cartDetails?.products.map((product)=> <tr key={product.product.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
         <td className="p-4">
           <img src={product.product.imageCover} className="w-16 md:w-32 max-w-full max-h-full" alt={product.product.title} />
@@ -133,7 +133,7 @@ export default function Cart() {
           </div>
         </td>
         <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-          {product.price} EGP
+          {product.count * product.price} EGP
         </td>
         <td className="px-6 py-4">
           <span  onClick={()=> deleteItem(product.product.id)} className="cursor-pointer font-medium text-red-600 dark:text-red-500 hover:underline">Remove</span>
